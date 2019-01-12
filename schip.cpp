43214f74f0279de9b889a815f6bb4d81f7af8ebe
JaydenWev <jaydenwev@gmail.com>
2019-01-12 16:44:46 +0100

    schip initial commit

---
+++ schip.cpp
@@ -0,0 +1,5 @@
+// hier moeten de eigenschappen van de schepen komen
+// grootte
+// nog "levend"
+int size;                   //1 is kleinste      9 is grootste
+int isAlive = true;         //true is in het veld       false is uit het spel
